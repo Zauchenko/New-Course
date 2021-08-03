31f58d94b155886bf08d4e1700a763f28a2b52e3
Oleg <zov.radek@gmail.com>
2021-08-03 11:08:39 +0300

    Test

--- First lesson/index.js
+++ First lesson/index.js
@@ -1,11 +1,1 @@
-const plus = (b) => {
-    let x = 1;
-    return y => {
-        x++
-        return x+y+b;
-    }
-}
-
-const add5 = plus(5);
-
-console.log(add5(1));
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
