ea75936bbdd87b00cb9fa8092db8a7ba778b6be3
Oleg <zov.radek@gmail.com>
2021-08-04 18:47:27 +0300

    Update script.js

--- Second lesson/js/script.js
+++ Second lesson/js/script.js
@@ -25,15 +25,11 @@ const movieDB = {
 };
 
 document.querySelectorAll('.promo__adv img').forEach(item => item.remove());
-
 document.querySelector('.promo__genre').textContent = 'драма';
-
 document.querySelector('.promo__bg').style.backgroundImage = 'url("../img/bg.jpg")';
-
 
 const oldFilmList = document.querySelector('.promo__interactive-list');
 oldFilmList.innerHTML = '';
-
 
 movieDB.movies.sort().forEach((item, index) => {
     oldFilmList.innerHTML += `<li class="promo__interactive-item">${index + 1}. ${item}
